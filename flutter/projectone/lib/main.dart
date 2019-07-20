5459b95ee50547be063c70a2f844295caaa605ff
Nattaphon Jeamjit <nattaphon.jeamjit@gmail.com>
2019-07-20 11:36:44 +0700

    [Javaverse] Flutter - added theme

--- flutter/projectone/lib/main.dart
+++ flutter/projectone/lib/main.dart
@@ -1,13 +1,3 @@
-// Flutter code sample for material.BottomNavigationBar.1
-
-// This example shows a [BottomNavigationBar] as it is used within a [Scaffold]
-// widget. The [BottomNavigationBar] has three [BottomNavigationBarItem]
-// widgets and the [currentIndex] is set to index 0. The selected item is
-// amber. The `_onItemTapped` function changes the selected item's index
-// and displays a corresponding message in the center of the [Scaffold].
-//
-// ![A scaffold with a bottom navigation bar containing three bottom navigation
-// bar items. The first one is selected.](https://flutter.github.io/assets-for-api-docs/assets/material/bottom_navigation_bar.png)
 
 import 'package:flutter/material.dart';
 
@@ -15,12 +5,16 @@ void main() => runApp(MyApp());
 
 /// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
-	static const String _title = 'Flutter Code Sample';
+	static const String _title = 'Dutchmill Stock Checker';
 	
 	@override
 	Widget build(BuildContext context) {
 		return MaterialApp(
+			debugShowCheckedModeBanner: false,
 			title: _title,
+			theme: ThemeData(
+				primarySwatch: Colors.lightBlue
+			),
 			home: MyStatefulWidget(),
 		);
 	}
--- flutter/projectone/lib/util/common_util.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-// wrap component with GestureDetector
-// GestureDetector(onTap:(){dismissKeyboard(context)})
-dismissKeyboard(BuildContext context) {
-	FocusScope.of(context).requestFocus(new FocusNode());
-}
